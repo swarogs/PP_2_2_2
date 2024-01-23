25d062245be5681f2b0932acc471e92929c8ea11
swarogs <swarog664@gmail.com>
2024-01-23 21:19:13 +0300

    First Commit

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,22 @@
+package web.service;
+
+import web.model.Car;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImpl implements CarService {
+
+    @Override
+    public List<Car> carList(Integer count) {
+        List<Car> carList; {
+            carList = new ArrayList<>();
+            carList.add(new Car("Лада", "Кайора", 1997));
+            carList.add(new Car("Волга", "3000", 2000));
+            carList.add(new Car("Феррари", "Спорт", 2019));
+            carList.add(new Car("BMW", "Supreme", 2007));
+            carList.add(new Car("ЗиЛ", "АКПМ", 1980));
+        }
+        return carList.stream().limit(count).collect(Collectors.toList());
+    }
+}
