5f4a134f9fe4f6d460f16691cf3fb60dc94e4b84
swarogs <swarog664@gmail.com>
2024-01-24 19:59:54 +0300

    Third Commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -3,13 +3,9 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
-import java.util.List;
 
 @Controller
 public class CarsController {
