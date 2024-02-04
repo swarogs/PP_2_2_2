02fb3f92333b46b866957bc3fb2841e2b6c22ea2
swarogs <swarog664@gmail.com>
2024-02-04 12:32:28 +0300

    5 Commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -9,7 +9,6 @@ import web.service.CarService;
 
 @Controller
 public class CarsController {
-
     private final CarService carService;
 
     @Autowired
--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @Controller
 public class HelloController {
-
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
 		List<String> messages = new ArrayList<>();
